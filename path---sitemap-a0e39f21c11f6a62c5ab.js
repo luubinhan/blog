3b6c794e944e8b79cbb4acd603104fd0b72b69d0
An Luu <an.luu@idealogic.com.vn>
2017-08-02 16:12:59 +0700

    Updates

--- path---sitemap-a0e39f21c11f6a62c5ab.js
+++ path---sitemap-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([14],{"./node_modules/json-loader/index.js!./.cache/json/sitemap.json":function(o,e){o.exports={pathContext:{}}}});
+webpackJsonp([15],{"./node_modules/json-loader/index.js!./.cache/json/sitemap.json":function(o,e){o.exports={pathContext:{}}}});
 //# sourceMappingURL=path---sitemap-a0e39f21c11f6a62c5ab.js.map
