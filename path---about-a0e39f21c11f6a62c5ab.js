e18a2f79f09f5219543fb99a95d99d5440661966
An Luu <an.luu@idealogic.com.vn>
2018-06-08 13:09:21 +0700

    Updates

--- path---about-a0e39f21c11f6a62c5ab.js
+++ path---about-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([23],{"./node_modules/json-loader/index.js!./.cache/json/about.json":function(o,n){o.exports={pathContext:{}}}});
+webpackJsonp([24],{"./node_modules/json-loader/index.js!./.cache/json/about.json":function(o,n){o.exports={pathContext:{}}}});
 //# sourceMappingURL=path---about-a0e39f21c11f6a62c5ab.js.map
