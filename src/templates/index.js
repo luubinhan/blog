6b2ad54a4fc2fdc0626989c27dd874e3454a7182
An Luu <an.luu@idealogic.com.vn>
2018-07-02 15:23:05 +0700

    +

---
+++ src/templates/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Link from "gatsby-link";
+
+const NavLink = props => {
+  if (!props.test) {
+    return <Link to={props.url}>{props.text}</Link>;
+  } else {
+    return <span>{props.text}</span>;
+  }
+};
+
+const IndexPage = ({ data, pathContext }) => {
+  const { group, index, first, last, pageCount } = pathContext;
+  const previousUrl = index - 1 == 1 ? "" : (index - 1).toString();
+  const nextUrl = (index + 1).toString();
+
+  return (
+    <div>
+      <h4>{pageCount} Posts</h4>
+
+      {group.map(({ node }) => (
+        <div key={node.id} className="blogListing">
+          <div className="date">{node.frontmatter.date}</div>
+          <Link className="blogUrl" to={node.fields.slug}>
+            {node.frontmatter.title}
+          </Link>
+          <div>{node.excerpt}</div>
+        </div>
+      ))}
+      <div className="previousLink">
+        <NavLink test={first} url={previousUrl} text="Go to Previous Page" />
+      </div>
+      <div className="nextLink">
+        <NavLink test={last} url={nextUrl} text="Go to Next Page" />
+      </div>
+    </div>
+  );
+};
+export default IndexPage;
