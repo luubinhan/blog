9b28fe281543f981a9f924cc5d180627c941103e
Lưu Bình An <anlb@blockchainlabs.asia>
2019-03-27 15:02:50 +0700

    update zalo pay

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -15,7 +15,7 @@ export default function Bio({ img = "", href = "", name = "", desc = "" }) {
           <div className="bio-desc">{desc}</div>
         </div>
       </GatsbyLink>
-      <a className="btn btn-primary mt-10" style={{color: '#fff', fontWeight: 'bold'}} href="https://www.patreon.com/luubinhan" rel="noopener noreferrer" target="_blank">Ủng hộ bằng Patreon</a>
+      <a className="btn btn-primary mt-10" style={{color: '#fff', fontWeight: 'bold'}} href="http://luubinhan.github.io/blog/donate" rel="noopener noreferrer" target="_blank">Hỗ trợ bằng Zalo Pay</a>
     </div>
   );
 }
