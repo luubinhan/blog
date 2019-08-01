a67c94deeef50102536b9423dac638383f1a857b
Lưu Bình An <luubinhan1989@gmail.com>
2019-08-01 22:13:20 +0700

    Updates

--- facebookcomment.js
+++ facebookcomment.js
@@ -1,7 +1,4 @@
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.12&appId=445556375516928&autoLogAppEvents=1';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+(adsbygoogle = window.adsbygoogle || []).push({
+  google_ad_client: 'ca-pub-5409528808847700',
+  enable_page_level_ads: true
+});
