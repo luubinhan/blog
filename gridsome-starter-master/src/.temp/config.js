3fa8aa5df15cbdc21e75095a8d9c3d07346cf5fe
Lưu Bình An <luubinhan1989@gmail.com>
2020-09-20 09:56:09 +0700

    update gridsome

--- gridsome-starter-master/src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Website Name",
-  "siteUrl": "#",
-  "version": "0.7.17",
-  "catchLinks": true
-}
--- gridsome-starter-master/src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1594634014142
