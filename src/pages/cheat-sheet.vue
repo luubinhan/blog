eca1d5a7d1cc981e5fd9f128b491b1adf4d6ad41
Lưu Bình An <luubinhan1989@gmail.com>
2020-10-09 20:21:09 +0700

    Create cheat-sheet.vue

---
+++ src/pages/cheat-sheet.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Hàm</th>
+            <th>Input</th>
+            <th>Ouput</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </Layout>
+</template>
