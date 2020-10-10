afdb46f17de9ccd049c724d8240e4aa8c8eff497
Lưu Bình An <luubinhan1989@gmail.com>
2020-10-10 18:13:42 +0700

    +

--- src/pages/cheat-sheet.vue
+++ src/pages/cheat-sheet.vue
@@ -1,14 +1,55 @@
 <template>
   <Layout>
     <div>
-      <table>
+      <h1 class="mb-20">Tra cứu nhanh</h1>
+      <h2 class="mb-10">Mảng</h2>
+      <table class="table">
         <thead>
           <tr>
-            <th>Hàm</th>
-            <th>Input</th>
-            <th>Ouput</th>
+            <th>Cần</th>
+            <th>Dùng hàm</th>
+            <th>Ví dụ</th>
           </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td>Merge</td>
+            <td><code>concat</code></td>
+            <td>a</td>
+          </tr>
+          <tr>
+            <td>Copy một cụm các phần tử</td>
+            <td><code>slice</code></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Cắt một cụm các phần tử</td>
+            <td><code>splice</code></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Lọc theo điều kiện</td>
+            <td><code>filter</code></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+      <h2 class="mb-10">Tính toán</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Cần</th>
+            <th>Dùng hàm</th>
+            <th>Ví dụ</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Lấy giá trị absolute</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
       </table>
     </div>
   </Layout>
