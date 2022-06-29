bc682261a93d22b04823d6c20e8f05eb9bf828d0
an.luu <an.luu@messagemedia.com>
2022-06-29 13:47:25 +0700

    comment

--- src/main.js
+++ src/main.js
@@ -2,13 +2,23 @@
 import '~/assets/style/index.scss'
 import '~/assets/fontello/css/down.css'
 import VueFacebook from '@p803/vue-facebook'
+import Vssue from 'vssue';
+import GithubV3 from '@vssue/api-github-v3';
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
+import 'vssue/dist/vssue.css'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
+  Vue.use(Vssue, {
+    api: GithubV3,
+    owner: process.env.OWNER_OF_REPO,
+    repo: process.env.NAME_OF_REPO,
+    clientId: process.env.YOUR_CLIENT_ID,
+    clientSecret: process.env.YOUR_CLIENT_SECRET,
+  })
   Vue.use(VueFacebook, {
     appId: '445556375516928', // required
     version: 'v8.0',   // required
